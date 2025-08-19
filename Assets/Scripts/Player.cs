aa0b02c275d0cecaff65133311fa2b6d04ddddb6
Gabriel Bertasso <gabrieljacintho868@gmail.com>
2025-08-19 10:36:04 -0400

    Setting Up A Scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private NetworkCharacterController _characterController;
+
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            data.Direction.Normalize(); // Prevent cheating
+            _characterController.Move(data.Direction * 5f * Runner.DeltaTime);
+        }
+    }
+}
