49074928ad938a5ee1fb6f76f1742027edd08587
Gabriel Bertasso <gabrieljacintho868@gmail.com>
2025-08-20 15:39:07 -0400

    Property Changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,18 @@ public class Player : NetworkBehaviour
     [SerializeField] private NetworkCharacterController _characterController;
     [SerializeField] private NetworkPrefabRef _ballPrefab;
     [SerializeField] private PhysxBall _physxBallPrefab;
+    
+    private ChangeDetector _changeDetector;
+    private Material _material;
 
     [Networked] private TickTimer FireDelay { get; set; }
+    [Networked] private bool SpawnedProjectile { get; set; }
 
+
+    private void Awake()
+    {
+        _material = GetComponentInChildren<Renderer>().material;
+    }
 
     public override void FixedUpdateNetwork()
     {
@@ -29,6 +38,7 @@ public class Player : NetworkBehaviour
                             @object.GetComponent<Ball>().Init();
                         });
 
+                    SpawnedProjectile = !SpawnedProjectile;
                 }
                 else if (data.Buttons.IsSet(NetworkInputData.MOUSEBUTTON1))
                 {
@@ -39,8 +49,41 @@ public class Player : NetworkBehaviour
                         {
                             @object.GetComponent<PhysxBall>().Init(transform.forward * 10f);
                         });
+
+                    SpawnedProjectile = !SpawnedProjectile;
                 }
             }
         }
+    }
+
+    public override void Spawned()
+    {
+        _changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState);
+    }
+
+    /* 
+     * This code iterates over all changes that happened to the Networked Properties since the last call to the ChangeDetector.
+     * So in this case since the last Render. If a change in the color value is detected on any client the MeshRenderer is updated.
+     * 
+     * The primary benefit of using change callbacks over common messages like RPCs is that the callback is executed immediately
+     * after the tick in which the value is changed, where as an RPC may arrive later when the game is in an completely different state.
+     * 
+     * The color should be updated in Render() as a linear interpolation from the current color towards blue.
+     * This is done in Render() rather than Update() because it is guaranteed to run after FixedUpdateNetwork()
+     * and it uses Time.deltaTime rather than Runner.DeltaTime because it is running in Unity's render loop and not as part of the Fusion simulation.
+     */
+    public override void Render()
+    {
+        foreach (var change in _changeDetector.DetectChanges(this))
+        {
+            switch (change)
+            {
+                case nameof(SpawnedProjectile):
+                    _material.color = Color.white;
+                    break;
+            }
+        }
+
+        _material.color = Color.Lerp(_material.color, Color.blue, Time.deltaTime);
     }
 }
